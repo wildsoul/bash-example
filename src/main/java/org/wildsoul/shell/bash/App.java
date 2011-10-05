adce700d4b1a1dc966f31292fa1aed08f50172d3
wildsoul <wildsoulmaxwell@gmail.com>
2011-10-06 00:23:51 +0100

    first

---
+++ src/main/java/org/wildsoul/shell/bash/App.java
@@ -0,0 +1,13 @@
+package org.wildsoul.shell.bash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
